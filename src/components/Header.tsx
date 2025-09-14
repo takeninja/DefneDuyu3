e6a72f27f4fd50826ee96f9584fd1d21bff0165b
Tayfun Yakut <40498284+takeninja@users.noreply.github.com>
2025-09-14 18:06:44 +0300

    Adjust header menu spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const Header = () => {
           </div>
 
           {/* Desktop Navigation */}
-          <nav className="hidden md:flex space-x-2">
+          <nav className="hidden md:flex space-x-8">
             <Link 
               to="/" 
               className={`transition-colors duration-200 font-medium ${
